97c4b26a01408d1deb9a79fa5cedcccc3fbbe013
chinmoyeedash <chinmoyeedash@gmail.com>
2017-08-09 12:50:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,28 +1,68 @@
-//Counter Code
+// //Counter Code
+// var button = document.getElementById('counter');
+// var counter = 0;
+
+// button.onclick = function() {
+
+// //create a request to counter endpoint
+
+// var request = new XMLHttpRequest();
+// request.onreadystatechange = function () {
+// if(request.readystate === XMLHttpRequest.DONE)
+// {
+// if(request.status===200)
+// {
+// var counter = request.responseText;
+// var span = document.getElementById('count');
+// span.innerHTML = counter.toString();
+// }
+// }
+// //not yet done - no action reqd
+// };
+
+// //make a request
+// request.open('GET','http://chinmoyeedash31.imad.hasura-app.io/counter',true);
+// request.send(null);
+
+// };
+// Counter code
 var button = document.getElementById('counter');
-var counter = 0;
 
-button.onclick = function() {
+button.onclick = function () {
 
-//create a request to counter endpoint
-
+// Create a request object-- XMLhttpRequest()
 var request = new XMLHttpRequest();
+
+// Capture the response and store it in a variable
 request.onreadystatechange = function () {
-if(request.readystate === XMLHttpRequest.DONE)
-{
-if(request.status===200)
-{
-var counter = request.responseText;
-var span = document.getElementById('count');
-span.innerHTML = counter.toString();
-}
-}
-//not yet done - no action reqd
+  if (request.readyState === XMLHttpRequest.DONE){
+      //Take some action
+      if (request.status === 200) {
+          var counter = request.responseText;
+          var span = document.getElementById('count');
+          span.innerHTML = counter.toString();
+      }
+  }   
+// Not done yet
 };
 
-//make a request
-request.open('GET','http://chinmoyeedash31.imad.hasura-app.io/counter',true);
+// Make the request
+request.open('GET', 'http://chinmoyeedash31.imad.hasura-app.io/counter', true);
 request.send(null);
-
 };
 
+// Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn'); 
+submit.onclick = function () {
+    // Make a request to the server and send the name
+    // Capture a list of names and render it as a list
+    var names = ['name1','name2','name3','name4'];
+    var list ='';
+    for (var i=0; i< names.length; i++) {
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};

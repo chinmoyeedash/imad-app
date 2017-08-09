cf374619000086cc90b27e8afd86f27a28338671
chinmoyeedash <chinmoyeedash@gmail.com>
2017-08-09 15:07:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -52,17 +52,38 @@ request.send(null);
 };
 
 // Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-var submit = document.getElementById('submit_btn'); 
-submit.onclick = function () {
+var nameInput = document.getElementById('nameinput');
+var newname = nameInput.value;
+var submitbtn = document.getElementById('submitbtn'); 
+submitbtn.onclick = function () {
     // Make a request to the server and send the name
     // Capture a list of names and render it as a list
-    var names = ['name1','name2','name3','name4'];
+    
+   /* var names = ['name1','name2','name3','name4'];
     var list ='';
     for (var i=0; i< names.length; i++) {
         list += '<li>' + names[i] + '</li>';
     }
     var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    ul.innerHTML = list;*/
+    
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function() {
+    if (request.readyState===XMLHttpRequest.DONE) {
+        if (request.status===200) {
+            var names=JSON.parse(request.responseText);
+            var list='';
+            for(var i=0;i<names.length;i++){
+                list=list + '<li>'+names[i]+'<li>';
+                
+            }
+            console.log(list);
+            var ul = document.getElementById('namelist');
+            ul.innerHTML = list;
+        }
+    }
+    };
+    request.send('GET','chinmoyeedash31.imad.hasura-app.io/submitbtn?name='+newname);
+    request.send(null);
 };
+        

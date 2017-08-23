2b1e2bdec9ebdb0ff8c2180386b8e2124ed3e586
chinmoyeedash <chinmoyeedash@gmail.com>
2017-08-23 23:32:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var Pool=require('pg').Pool;
 var config = {
     user: 'chinmoyeedash31',
     database: 'chinmoyeedash31',
-    host: 'localhost',
+    host: localhost,
     port: '5432',
     password: process.env.DB_PASSWORD
 };
